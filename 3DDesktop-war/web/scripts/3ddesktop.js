8729ced9720a5287905f7d50af1abec376264a50
Kanat Alimanov <paindefenderwashere@gmail.com>
2016-11-04 14:28:20 +0600

    Added WASD and arrows control

--- 3DDesktop-war/web/scripts/3ddesktop.js
+++ 3DDesktop-war/web/scripts/3ddesktop.js
@@ -115,8 +115,85 @@ function init(){
     newPaper.position.y = -7;
     scene.add(newPaper);
 
+    //raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
+
+
+    var onKeyDown = function ( event ) {
+
+      switch ( event.keyCode ) {
+
+        case 38: // up
+        case 87: // w
+          moveForward = true;
+          break;
+
+        case 37: // left
+        case 65: // a
+          moveLeft = true;
+          break;
+
+        case 40: // down
+        case 83: // s
+          moveBackward = true;
+          break;
+
+        case 39: // right
+        case 68: // d
+          moveRight = true;
+          break;
+
+        /*case 32: // space
+          if ( canJump === true ) velocity.y += 350;
+          canJump = false;
+          break;*/
+
+      }
+
+    };
+
+    var onKeyUp = function ( event ) {
+
+      switch( event.keyCode ) {
+
+        case 38: // up
+        case 87: // w
+          moveForward = false;
+          break;
+
+        case 37: // left
+        case 65: // a
+          moveLeft = false;
+          break;
+
+        case 40: // down
+        case 83: // s
+          moveBackward = false;
+          break;
+
+        case 39: // right
+        case 68: // d
+          moveRight = false;
+          break;
+
+      }
+
+    };
+    document.addEventListener( 'keydown', onKeyDown, false );
+    document.addEventListener( 'keyup', onKeyUp, false );
+
     window.addEventListener( 'resize', onWindowResize, false );
 }
+
+var controlsEnabled = false;
+
+var moveForward = false;
+var moveBackward = false;
+var moveLeft = false;
+var moveRight = false;
+//var canJump = false;
+
+var prevTime = performance.now();
+var velocity = new THREE.Vector3();
 
 function render() {
   renderer.render(scene, camera);
@@ -124,6 +201,52 @@ function render() {
 
 function animate() {
   requestAnimationFrame(animate);
+
+  if ( controlsEnabled ) {
+    // raycaster.ray.origin.copy( controls.getObject().position );
+    // raycaster.ray.origin.y -= 10;
+
+    //var intersections = raycaster.intersectObjects( objects );
+
+    //var isOnObject = intersections.length > 0;
+
+    var time = performance.now();
+    var delta = ( time - prevTime ) / 1000;
+
+    velocity.x -= velocity.x * 20.0 * delta;
+    velocity.z -= velocity.z * 20.0 * delta;
+
+    //velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+
+    if ( moveForward ) velocity.z -= 1200.0 * delta;
+    if ( moveBackward ) velocity.z += 1200.0 * delta;
+
+    if ( moveLeft ) velocity.x -= 1200.0 * delta;
+    if ( moveRight ) velocity.x += 1200.0 * delta;
+
+    /*if ( isOnObject === true ) {
+      velocity.y = Math.max( 0, velocity.y );
+
+      canJump = true;
+    }*/
+
+    controls.getObject().translateX( velocity.x * delta );
+    //controls.getObject().translateY( velocity.y * delta );
+    controls.getObject().translateZ( velocity.z * delta );
+
+    /*if ( controls.getObject().position.y < 10 ) {
+
+      velocity.y = 0;
+      controls.getObject().position.y = 10;
+
+      canJump = true;
+
+    }*/
+
+    prevTime = time;
+
+  }
+
   render();
 }
 
